2d83025ea8c3720d1137da138208438e19abf0a8
Conor Sheridan <c_sheridan@mailbox.org>
2024-08-14 18:58:30 +0100

    Update subband_plotter.py

--- subband_plotter.py
+++ subband_plotter.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-d',   dest='dir',   type=str,   help='directory of data to plot. Default is the scripts directory.',default='.')
-parser.add_argument('-n',   dest='nsub',  type=int,   help='number of subbands to plot. Deflaut = 1',default=1)
-parser.add_argument('-f',   dest='file',  type=str,   help='naming convetion of the file(s) with {0} where the number iteration is')
+parser.add_argument('-d',   dest='dir',   type=str,   help='directory of data to plot. Default your current directory.',default='.')
+parser.add_argument('-n',   dest='nsub',  type=int,   help='number of subbands to plot. Default = 1',default=1)
+parser.add_argument('-f',   dest='file',  type=str,   help='naming convention of the file(s) with {0} where the number iteration is. eg: datafile{0}.txt')
 args = parser.parse_args()
 data_dir = args.dir
 n = args.nsub
@@ -15,6 +15,7 @@ filename = args.file
 x_sets={}
 y_sets={}
 
+#labels for graph - can change
 freqsn=[107.71484375,118.65234375,129.58984375,140.52734375,151.46484375,162.40234375,173.33984375,184.27734375,193.65234375]
 
 for i in range(n):
@@ -30,9 +31,9 @@ for i in range(n):
         
     plt.plot(np.array(x_sets["x_set{0}".format(i)])/128, np.array(y_sets["y_set{0}".format(i)])/max(y_sets["y_set{0}".format(i)]), label='{0:.2f} MHz'.format(freqsn[i]))
 
-
 plt.xlabel('Phase')
 plt.ylabel('Flux')
 plt.xlim(0,1)
+plt.title("2024/07/23 B0531+21 Flux vs Phase ")
 plt.legend()
 plt.show()
